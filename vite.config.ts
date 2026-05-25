855c563cbfd616d536da5f663523a794372307c4
cckk-lucky <xizhiledao@163.com>
2026-05-25 23:19:26 +0800

    Fix: use relative base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/wxLayout/',
+  base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
